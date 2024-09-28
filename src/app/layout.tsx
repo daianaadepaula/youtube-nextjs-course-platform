a00f1e9a1526fb1bd9dd5f00dae8bbe521113806
daianaadepaula <daianaadepaula1@gmail.com>
2024-09-28 16:28:04 -0300

    feat: add title and description in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Nunito } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const nunito = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "NextJs plataforma de curso",
-  description: "Plataforma de curso online utilizando NextJs, TypeScript e TailwindCSS",
+  title: "CodarSe",
+  description: "Os melhores cursos de programação gratuitos coma melhor experiência de aprendizado e foco.",
 };
 
 interface IRootLayout extends Readonly<{ children: React.ReactNode }> {}
@@ -14,7 +14,7 @@ interface IRootLayout extends Readonly<{ children: React.ReactNode }> {}
 export default function RootLayout({ children }: IRootLayout) {
   return (
     <html lang="pt-Br">
-      <body className={inter.className}>{children}</body>
+      <body className={nunito.className}>{children}</body>
     </html>
   );
 }
